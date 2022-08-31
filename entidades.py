4d552359bd2a281187f9fd24b1deb88cf3071e79
Your Name <you@example.com>
2022-08-31 18:22:03 +0200

    Nueva versión con movimiento de raqueta1 y 2 con límites de pantalla

--- entidades.py
+++ entidades.py
@@ -24,9 +24,25 @@ class Raqueta:
         self.vx = 0
         self.vy = 0
 
-    def dibujar(self,pantalla):
+    def dibujar(self, pantalla):
         pg.draw.rect(pantalla, self.color, (self.center_x - self.w // 2, self.center_y - self.h //2, self.w, self.h))
         #Se hace la formula de rectarle 
+    
+    def mover(self,tecla_arriba,tecla_abajo, y_max=600):
+        estado_teclas = pg.key.get_pressed()
+        if estado_teclas[tecla_arriba]:
+            self.center_y -= self.vy
+        if self.center_y < self.h // 2:
+            self.center_y = self.h // 2
+
+        if estado_teclas[tecla_abajo]:
+            self.center_y += self.vy
+        if self.center_y > y_max - self.h // 2:
+            self.center_y = y_max - self.h // 2
+            
+
+
+
 
 
     
--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 import pygame as pg
 from entidades import Bola, Raqueta
 
-
-
 pg.init()
 
 pantalla_principal = pg.display.set_mode((800,600))
 pg.display.set_caption("PONG")
+cronometro = pg.time.Clock()
+
 
 bola = Bola(400, 300, color=(255,255,255))
 raqueta1 = Raqueta(20, 300, w=20, h=120, color=(255,255,255))
 raqueta2 = Raqueta(780,300, w=20, h=120, color=(255,255,255))
 raqueta2.vy = 5
+raqueta1.vy = 5
+
 game_over = False
 
 while not game_over:
+    dt = cronometro.tick()
     for evento in pg.event.get():
         #eventos que hace el usuario y lo captura pg.event.get() y lo devuelve una lista de eventos
         if evento.type == pg.QUIT:
             game_over = True
-    estado_teclas = pg.key.get_pressed()
-    if estado_teclas[pg.K_UP]:#se puede poner también if estado_teclas[pg.K_UP] == True:
-        raqueta2.center_y -= raqueta2.vy
-    if estado_teclas[pg.K_DOWN]:#se puede poner también if estado_teclas[pg.K_DOWN] == True:
-        raqueta2.center_y += raqueta2.vy
-
+    raqueta2.mover(pg.K_UP, pg.K_DOWN)
+    raqueta1.mover(pg.K_a, pg.K_z)
+    
 
     pantalla_principal.fill((0, 0, 0))
     bola.dibujar(pantalla_principal)
